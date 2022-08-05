5a043053e559801eb0a4ec12db5e2d38426c4912
radameshk <scorphylum@gmail.com>
2022-08-05 11:12:47 -0500

    buiding task list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {TaskCreator} from './component/TaskCreator'
 
 function App() {
+
+  const [ taskItems, setTasksItems] = useState ([
+    {name: 'mi primer tarea', done: false},
+    {name: 'mi segunda tarea', done: false},
+    {name: 'mi tercer tarea', done: false},
+  ])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <TaskCreator />
+
+
+      {
+        taskItems.map((task,index) => (
+          <div key={index}>
+            {task.name}
+            </div>
+        ))
+      }
     </div>
   );
 }
---
+++ src/component/TaskCreator.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const TaskCreator = () => {
+  const [newTaskName, setNewTaskName] = useState()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("tasks", newTaskName);
+    setNewTaskName("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter a New Task"
+        value={newTaskName}
+        onChange={(e) => setNewTaskName(e.target.value)} />
+
+      <button>Save Task</button>
+    </form>
+  )
+
+}
